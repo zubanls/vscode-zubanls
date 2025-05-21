0b1c90a52e22623543fdba617d183f3580d59742
Dave Halter <davidhalter88@gmail.com>
2025-05-21 12:27:16 +0200

    First extension with code

---
+++ DEVELOPMENT.md
@@ -0,0 +1,11 @@
+# Development
+
+## Publish a Release
+
+1. Upgrade the version in `package.json`.
+
+2. Run the following code
+```
+vsce package
+vsce publish
+```
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from 'vscode-languageclient/node';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let client: LanguageClient;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "zubanls" is now active!');
+export function activate() {
+  const serverOptions: ServerOptions = {
+    command: "zubanls",
+    args: [],
+  };
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('zubanls.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ZubanLS!');
-	});
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'python' }],
+  };
 
-	context.subscriptions.push(disposable);
+  client = new LanguageClient(
+    'zubanls',
+    'ZubanLS',
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
+
